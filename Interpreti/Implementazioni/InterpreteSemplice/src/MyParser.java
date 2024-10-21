1327cb003cbf79e47118dab6168bd1b9ce9a8175
kevin <kevinkoltraka1011@gmail.com>
2024-10-21 19:29:38 +0200

    asd

--- Interpreti/Implementazioni/InterpreteSemplice/src/MyParser.java
+++ Interpreti/Implementazioni/InterpreteSemplice/src/MyParser.java
@@ -9,104 +9,88 @@ public class MyParser {
     }
 
     public int parseExp() {
-System.out.println("parseEXP:\tcurrent token = " + this.currentToken.toString());   
         int t1 = parseTerm();
-System.out.println("parseEXP:\tt1 = " + t1);   
 
         while (currentToken != null) {
             if( currentToken.equals("+") ) {
                 currentToken = scanner.getNextToken();
-System.out.println("parseEXP:\ttrovato + ! next token = " + this.currentToken.toString());   
 
                 int t2 = parseTerm();
-System.out.println("parseEXP:\tt2 = " + t2);   
+                System.out.print("\tValuto "+t1+" + "+t2+" = ");
                 t1 = t1 + t2;
+                System.out.println(t1);
             }
             else if( currentToken.equals("-") ) {
                 currentToken = scanner.getNextToken();
-System.out.println("parseEXP:\ttrovato - ! nexto token = " + this.currentToken.toString());   
 
                 int t2 = parseTerm();
-System.out.println("parseEXP:\tt2 = " + t2);   
 
+                System.out.print("\tValuto "+t1+" - "+t2+" = ");
                 t1 = t1 - t2;
+                System.out.println(t1);
             }
             else {
-System.out.println("parseEXP:\ttrovato qualcosa che non riconosco");   
                 return t1; // next token non fa parte di L(Exp)
             }
         } 
 
-System.out.println("parseEXP:\tfiniti i token");   
         return t1; // next token nullo -> end input
     }
 
     public int parseTerm() {
-System.out.println("parseTERM:\tcurrent token = " + this.currentToken.toString());   
         int f1 = parseFactor();
-System.out.println("parseTERM:\tf1 = " + f1);   
 
         while (currentToken != null) {
             if( currentToken.equals("*") ) {
                 currentToken = scanner.getNextToken();
-System.out.println("parseTERM:\ttrovato * ! next token = " + this.currentToken.toString());   
 
                 int f2 = parseFactor();
-System.out.println("parseTERM:\tf2 = " + f2);   
 
+                System.out.print("\tValuto "+f1+" * "+f2+" = ");
                 f1 = f1 * f2;
+                System.out.println(f1);
             }
             else if( currentToken.equals(":") ) {
                 currentToken = scanner.getNextToken();
-System.out.println("parseTERM:\ttrovato : ! next token = " + this.currentToken.toString());   
 
                 int f2 = parseFactor();
-System.out.println("parseTERM:\tf2 = " + f2);   
                 
+                System.out.print("\tValuto "+f1+" / "+f2+" = ");
                 f1 = f1 / f2;
+                System.out.println(f1);
             }
             else {
-System.out.println("parseTERM:\ttrovato qualcosa che non riconosco");   
-
                 return f1; // next token non fa parte di L(Term)
             }
         }
 
-System.out.println("parseTERM:\tfiniti i token");   
         return f1; // next token nullo -> end input
     }
 
     public int parseFactor() {
-System.out.println("parseFACTOR:\tcurrent token = " + this.currentToken.toString());   
         if (currentToken.equals("(")) {
             currentToken = scanner.getNextToken();
-System.out.println("parseFACTOR:\ttrovata ( ! next token = " + this.currentToken.toString());   
             int innerExp = parseExp(); // self-embedding
-System.out.println("parseFACTOR:\tinnerExp is " + innerExp);   
 
-            if (currentToken.equals(")")) {
+            if (currentToken != null && currentToken.equals(")")) {
                 currentToken = scanner.getNextToken();
-if( currentToken != null)
-System.out.println("parseFACTOR:\ttrovata ) ! next token = " + this.currentToken.toString());   
                 return innerExp; // parentesi irrilevanti
             } 
             else {
-System.out.println("parseFACTOR:\tmanca ) ! ritorno false");   
                 throw new IllegalArgumentException("missing )");
             }
         }
         else { // dev’essere un numero
             if (currentToken.isNumber()) {
                 int value = currentToken.getAsInt();
+                System.out.println("\tValuto "+value);
+
                 currentToken = scanner.getNextToken();
-if( currentToken != null)
-System.out.println("parseFACTOR:\tnumero riconosciuto! next token = " + this.currentToken.toString());   
 
                 return value;
             }
             else {// non è un fattore, quindi
-System.out.println("parseFACTOR:\ttoken non riconosciuto!");   
-                throw new IllegalArgumentException(currentToken + "not a number");
+                throw new IllegalArgumentException(currentToken + " not a number");
             }
         }
     }
